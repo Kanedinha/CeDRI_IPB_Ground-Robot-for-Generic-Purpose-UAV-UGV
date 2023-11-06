04bcefb93a5f84b6a4351e7fbf6fdacf8778d7b9
Kanedinha <emerson.k337@gmail.com>
2023-11-06 12:13:00 +0000

    wtd timer fixed

--- SW/MotorController/MotorController/main/main.c
+++ SW/MotorController/MotorController/main/main.c
@@ -53,7 +53,7 @@
 // #define DEBUG
 
 #define ENCODER_A GPIO_NUM_25
-#define ENCODER_B GPIO_NUM_17
+#define ENCODER_B GPIO_NUM_26
 #define MOTOR_A1 GPIO_NUM_16
 #define MOTOR_A2 GPIO_NUM_27
 
@@ -64,7 +64,16 @@
 
 #define ESP_INTR_FLAG_DEFAULT 0
 
+#ifdef DEBUG
+long long int Timer1 = 0;
+long long int Timer2 = 0;
+long long int diff = 0;
+#endif
+
 const char *TAG = "CeDRI";
+uint8_t sig_a = 0, sig_b = 0, sig_ant_a = 0, sig_ant_b = 0;
+uint8_t dir_rot = 0;
+uint16_t angle = 0;
 
 const uint8_t enc_table[16] = {ENCODER_STOP, COUNTER_CLOCKWISE, CLOCKWISE, ENCODER_ERROR,
                                CLOCKWISE, ENCODER_STOP, ENCODER_ERROR, COUNTER_CLOCKWISE,
@@ -74,12 +83,13 @@ const uint8_t enc_table[16] = {ENCODER_STOP, COUNTER_CLOCKWISE, CLOCKWISE, ENCOD
 TaskHandle_t Motor_Direction_Handle = NULL;
 TaskHandle_t Encoder_Read_Handle = NULL;
 
-static QueueHandle_t gpio_evt_queue = NULL;
+// static QueueHandle_t gpio_evt_queue = NULL;
 
-static void IRAM_ATTR Encoder_Read_interrupt(void *arg)
+void IRAM_ATTR encoder_isr_handler(void *arg)
 {
-    uint32_t gpio_num = (uint32_t)arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+    sig_a = gpio_get_level(ENCODER_A);
+    sig_b = gpio_get_level(ENCODER_B);
+    xTaskResumeFromISR(Encoder_Read_Handle);
 }
 
 void Motor_Direction(void *args)
@@ -91,97 +101,63 @@ void Motor_Direction(void *args)
         gpio_set_level(MOTOR_A1, dir);
         gpio_set_level(MOTOR_A2, !dir);
         dir = !dir;
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        vTaskDelay(pdMS_TO_TICKS(1000));
 
         gpio_set_level(MOTOR_A1, 0);
         gpio_set_level(MOTOR_A2, 0);
-        vTaskDelay(500 / portTICK_RATE_MS);
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
 void Encoder_Read(void *args)
 {
-    bool sig_a = 0, sig_b = 0, sig_ant_a = 0, sig_ant_b = 0;
-    uint8_t dir_rot = 0;
-    uint16_t angle = 0;
-    uint32_t io_num;
-
     while (1)
     {
-
-#ifdef DEBUG
-        long long int Timer1 = esp_timer_get_time();
-        ets_delay_us(15);
-        long long int Timer2 = esp_timer_get_time();
-        long long int diff = Timer2 - Timer1;
-        ESP_LOGI(TAG, "Sample period: %lld μs", diff);
-#endif
-
-        sig_a = gpio_get_level(ENCODER_A);
-        sig_b = gpio_get_level(ENCODER_B);
 
         if ((sig_a != sig_ant_a) || (sig_b != sig_ant_b))
         {
             dir_rot = (sig_a << 3) | (sig_b << 2) | (sig_ant_a << 1) | (sig_ant_b);
+#ifndef DEBUG
             ESP_LOGI(TAG, "Enc A: %d - Enc B: %d - wise: %d", sig_a, sig_b, enc_table[dir_rot]);
+#endif
+            sig_ant_a = sig_a;
+            sig_ant_b = sig_b;
+
+#ifdef DEBUG
+            Timer2 = esp_timer_get_time();
+            diff = Timer2 - Timer1;
+            ESP_LOGI(TAG, "Sample period: %lld μs", diff);
+            Timer1 = esp_timer_get_time();
+#endif
+        }
+        else{
+            vTaskSuspend(NULL);
         }
 
-        sig_ant_a = sig_a;
-        sig_ant_b = sig_b;
     }
 }
 
 void app_main(void)
 {
     // Disable Watchdog interrupt
-    rtc_wdt_protect_off();
-    rtc_wdt_disable();
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    io_conf.pin_bit_mask = (1ULL << ENCODER_A) | (1ULL << ENCODER_B);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    gpio_config(&io_conf);
 
-    esp_err_t err = 0;
-
-    err = gpio_set_direction(ENCODER_A, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(ENCODER_A, GPIO_PULLDOWN_ONLY);
-    gpio_set_intr_type(ENCODER_A, GPIO_INTR_ANYEDGE);
-
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "error in I/O configuration");
-    }
-
-    err = gpio_set_direction(ENCODER_B, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(ENCODER_B, GPIO_PULLDOWN_ONLY);
-    gpio_set_intr_type(ENCODER_B, GPIO_INTR_ANYEDGE);
-
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "error in I/O configuration");
-    }
-
-    err = gpio_set_direction(MOTOR_A1, GPIO_MODE_OUTPUT);
-
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "error in I/O configuration");
-    }
-
-    err = gpio_set_direction(MOTOR_A2, GPIO_MODE_OUTPUT);
-
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "error in I/O configuration");
-    }
-
-    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_isr_handler_add(ENCODER_A, Encoder_Read_interrupt, (void *)ENCODER_A);
-    gpio_isr_handler_add(ENCODER_B, Encoder_Read_interrupt, (void *)ENCODER_B);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(ENCODER_A, encoder_isr_handler, NULL);
+    gpio_isr_handler_add(ENCODER_B, encoder_isr_handler, NULL);
 
     xTaskCreate(Motor_Direction, "MotorDirection", 4096, NULL, 10, &Motor_Direction_Handle);
     xTaskCreate(Encoder_Read, "Encoder_Read", 4096, NULL, 10, &Encoder_Read_Handle);
 
     while (1)
     {
-        vTaskDelay(100 / portTICK_RATE_MS);
+        // Leia encoder_count para determinar a posição do motor
+        vTaskDelay(pdMS_TO_TICKS(100)); // Aguarde um curto período entre as leituras
     }
 }
